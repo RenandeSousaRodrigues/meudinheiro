51b706c2518d247342f58164cb09269a405f8345
renan rodrigues <renandesousarodrigues@gmail.com>
2022-09-05 16:46:37 -0300

    corrigindo erros

--- apps/geral/urls.py
+++ apps/geral/urls.py
@@ -6,7 +6,7 @@ from geral import views
 
 urlpatterns=[
     path('nova-categoria/', views.nova_categoria, name='nova_categoria'),
-    path('categorias/', views.lista_categorias, name='Lista_categorias'),
+    path('categorias/', views.lista_categorias, name='lista_categorias'),
     path('', views.principal, name='principal'),#sempre será o ultimo pq ele tem a permissão maior que as demais
 
 ]
